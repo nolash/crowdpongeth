8aa778dd81e5c470ab1257a453c4c31822c429dc
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-09-08 16:44:46 +0200

    TODO: Current status of frontend refactoring. Watchout index2.html not runnable

--- frontend/src/pong2.js
+++ frontend/src/pong2.js
@@ -11,9 +11,33 @@ class State {
 }
 
 class Ball {
+  constructor (x, y, vx, vy) {
+    this.x = x
+    this.y = y
+    this.vx = vx
+    this.vy = vy
+    this.width = 4
+    this.height = 4
+  }
+
+  draw (p) {
+    p.fillRect(this.x, this.y, this.width, this.height)
+  }
+
+  update () {
+    this.x += this.vx
+    this.y += this.vy
+  }
+}
+
+class Display {
   constructor (x, y) {
     this.x = x
     this.y = y
+    this.value = 0
+  }
+  draw (p) {
+    p.fillText(this.value, this.x, this.y)
   }
 }
 
@@ -32,6 +56,18 @@ class KeyListener {
     document.addEventListener('keydown', this.keydown.bind(this))
     document.addEventListener('keyup', this.keyup.bind(this))
   }
+
+  isPressed (key) {
+    return this.pressedKeys[key] ? true : false
+  }
+
+  addKeyPressListener (keyCode, callback) {
+    document.addEventListener('keypress', function (e) {
+      if (e.keyCode === keyCode) {
+        callback(e)
+      }
+    })
+  }
 }
 
 class Paddle {
@@ -39,6 +75,13 @@ class Paddle {
     this.x = x
     this.y = y
     this.players = players // []
+    this.width = 2
+    this.height = 28
+    this.score = 0
+  }
+
+  draw (p) {
+    p.fillRect(this.x, this.y, this.width, this.height)
   }
 }
 
@@ -56,9 +99,7 @@ class Game {
     this.height = canvas.height
     this.context = canvas.getContext('2d')
     this.context.fillStyle = 'white'
-
     this.keys = new KeyListener()
-
     // Paddle 1
     this.p1 = new Paddle(5, 0)
     this.p1.y = this.height / 2 - this.p1.height / 2
@@ -88,67 +129,49 @@ class Game {
     this.display2.draw(this.context)
   }
 
+  score (p) {
+    // player scores
+    p.score++
+    var player = p === this.p1 ? 0 : 1
+
+    // set ball position
+    this.ball.x = this.width / 2
+    this.ball.y = p.y + p.height / 2
+
+    // set ball velocity
+    this.ball.vy = Math.floor(Math.random() * 12 - 6)
+    this.ball.vx = 7 - Math.abs(this.ball.vy)
+    if (player === 1) {
+      this.ball.vx *= -1
+    }
+  }
+
   start () {
     // based on block heights
     // TODO
   }
 
-  /// PRIVATE
-  initializeKeyListener () {
+  update () {
+    this.ball.update()
+    this.display1.value = this.p1.score
+    this.display2.value = this.p2.score
 
-  }
+    // To which Y direction the paddle is moving
+    if (this.keys.isPressed(83)) { // DOWN
+      this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4)
+    } else if (this.keys.isPressed(87)) { // UP
+      this.p1.y = Math.max(0, this.p1.y - 4)
+    }
 
-  // choose player 1 or player 2
-}
-// Initialize our game instance
-const game = new Game()
+    if (this.keys.isPressed(40)) { // DOWN
+      this.p2.y = Math.min(this.height - this.p2.height, this.p2.y + 4)
+    } else if (this.keys.isPressed(38)) { // UP
+      this.p2.y = Math.max(0, this.p2.y - 4)
+    }
 
-// TODO
-function MainLoop () {
-  // game.update()
-  // game.draw()
-  // drawTime()
-  // Call the main loop again at a frame rate of 30fps
-  setTimeout(MainLoop, 100)
-}
-
-// Start the game execution
-MainLoop()
-
-
-Game.prototype.draw = function () {
-  this.context.clearRect(0, 0, this.width, this.height)
-  this.context.fillRect(this.width / 2, 0, 2, this.height)
-
-  this.ball.draw(this.context)
-
-  this.p1.draw(this.context)
-  this.p2.draw(this.context)
-  this.display1.draw(this.context)
-  this.display2.draw(this.context)
-}
-
-Game.prototype.update = function () {
-  this.ball.update()
-  this.display1.value = this.p1.score
-  this.display2.value = this.p2.score
-
-  // To which Y direction the paddle is moving
-  if (this.keys.isPressed(83)) { // DOWN
-    this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4)
-  } else if (this.keys.isPressed(87)) { // UP
-    this.p1.y = Math.max(0, this.p1.y - 4)
-  }
-
-  if (this.keys.isPressed(40)) { // DOWN
-    this.p2.y = Math.min(this.height - this.p2.height, this.p2.y + 4)
-  } else if (this.keys.isPressed(38)) { // UP
-    this.p2.y = Math.max(0, this.p2.y - 4)
-  }
-
-  if (this.ball.vx > 0) {
-    if (this.p2.x <= this.ball.x + this.ball.width &&
-      this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
+    if (this.ball.vx > 0) {
+      if (this.p2.x <= this.ball.x + this.ball.width &&
+        this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
         var collisionDiff = this.ball.x + this.ball.width - this.p2.x
         var k = collisionDiff / this.ball.vx
         var y = this.ball.vy * k + (this.ball.y - this.ball.vy)
@@ -185,110 +208,23 @@ Game.prototype.update = function () {
       this.score(this.p2)
     }
   }
+}
+// TODO:  choose player 1 or player 2
+// Initialize our game instance
+const game = new Game()
 
-  Game.prototype.score = function (p) {
-    // player scores
-    p.score++
-    var player = p === this.p1 ? 0 : 1
+function drawTime () {
+  document.getElementById('timeBox').innerHTML = Math.round(new Date().getTime() / 1000)
+}
 
-    // set ball position
-    this.ball.x = this.width / 2
-    this.ball.y = p.y + p.height / 2
+// TODO
+function MainLoop () {
+  // game.update()
+  // game.draw()
+  drawTime()
+  // Call the main loop again at a frame rate of 30fps
+  setTimeout(MainLoop, 100)
+}
 
-    // set ball velocity
-    this.ball.vy = Math.floor(Math.random() * 12 - 6)
-    this.ball.vx = 7 - Math.abs(this.ball.vy)
-    if (player === 1) {
-      this.ball.vx *= -1
-    }
-  }
-
-
-  // PADDLE
-  function Paddle (x, y) {
-    this.x = x
-    this.y = y
-    this.width = 2
-    this.height = 28
-    this.score = 0
-  }
-
-  Paddle.prototype.draw = function (p) {
-    p.fillRect(this.x, this.y, this.width, this.height)
-  }
-
-  // BALL
-  function Ball () {
-    this.x = 0
-    this.y = 0
-    this.vx = 0
-    this.vy = 0
-    this.width = 4
-    this.height = 4
-  }
-
-  Ball.prototype.update = function () {
-    this.x += this.vx
-    this.y += this.vy
-  }
-
-  Ball.prototype.draw = function (p) {
-    p.fillRect(this.x, this.y, this.width, this.height)
-  }
-
-  // DISPLAY
-  function Display (x, y) {
-    this.x = x
-    this.y = y
-    this.value = 0
-  }
-
-  Display.prototype.draw = function (p) {
-    p.fillText(this.value, this.x, this.y)
-  }
-
-  // KEY LISTENER
-  function KeyListener () {
-    this.pressedKeys = []
-
-    this.keydown = function (e) {
-      this.pressedKeys[e.keyCode] = true
-    }
-
-    this.keyup = function (e) {
-      this.pressedKeys[e.keyCode] = false
-    }
-
-    document.addEventListener('keydown', this.keydown.bind(this))
-    document.addEventListener('keyup', this.keyup.bind(this))
-  }
-
-  KeyListener.prototype.isPressed = function (key) {
-    return this.pressedKeys[key] ? true : false
-  }
-
-  KeyListener.prototype.addKeyPressListener = function (keyCode, callback) {
-    document.addEventListener('keypress', function (e) {
-      if (e.keyCode === keyCode) {
-        callback(e)
-      }
-    })
-  }
-
-  function drawTime () {
-    document.getElementById('timeBox').innerHTML = Math.round(new Date().getTime() / 1000)
-  }
-
-  // Initialize our game instance
-  var game = new Game()
-
-  function MainLoop () {
-    game.update()
-    game.draw()
-    drawTime()
-    // Call the main loop again at a frame rate of 30fps
-    setTimeout(MainLoop, 100)
-  }
-
-  // Start the game execution
-  MainLoop()
+// Start the game execution
+MainLoop()
