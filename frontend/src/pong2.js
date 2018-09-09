4075264dc633ca90af029dbd8b42756ef932dd4d
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-09-09 02:19:02 +0200

    Finalize pong. Ready for web integration

--- frontend/src/pong2.js
+++ frontend/src/pong2.js
@@ -1,24 +1,3 @@
-class State {
-  constructor (timeStamp) {
-    this.ball = {}
-    this.paddlePlayer1 = {}
-    this.paddlePlayer2 = {}
-    this.timeStamp = timeStamp
-  }
-
-  setBall (ball) {
-    this.ball = ball
-  }
-
-  setPaddlePlayer1 (paddle) {
-    this.paddlePlayer1 = paddle
-  }
-
-  setPaddlePlayer2 (paddle) {
-    this.paddlePlayer2 = paddle
-  }
-}
-
 class Ball {
   constructor (x, y, vx, vy) {
     this.x = x
@@ -33,7 +12,7 @@ class Ball {
     p.fillRect(this.x, this.y, this.width, this.height)
   }
 
-  updatePosition () {
+  setNextPosition () {
     this.x += this.vx
     this.y += this.vy
   }
@@ -43,14 +22,14 @@ class Ball {
   }
 }
 
-class Display {
+class ValueDisplay {
   constructor (x, y) {
     this.x = x
     this.y = y
     this.value = 0
   }
-  draw (p) {
-    p.fillText(this.value, this.x, this.y)
+  draw (context) {
+    context.fillText(this.value, this.x, this.y)
   }
 }
 
@@ -95,21 +74,23 @@ class Paddle {
   constructor (x, y, players) {
     this.x = x
     this.y = y
-    this.players = players // []
+    this.players = players // [] //TODO: Do we really need that
     this.width = 2
     this.height = 28
     this.score = 0
   }
 
-  draw (p) {
-    p.fillRect(this.x, this.y, this.width, this.height)
+  draw (context) {
+    context.fillRect(this.x, this.y, this.width, this.height)
   }
 }
 
 class Player {
-  constructor (isPlayer1, name) {
-    this.player = isPlayer1 ? 1 : 2
+  constructor (playerNumber, name, paddle) {
+    this.playerNumber = playerNumber
     this.name = name
+    this.paddle = paddle
+    this.score = 0
   }
 }
 
@@ -121,34 +102,44 @@ class Game {
     this.context = canvas.getContext('2d')
     this.context.fillStyle = 'white'
     this.keys = new KeyListener()
-    // Paddle 1
-    this.p1 = this.getInitialPaddle1()
-    // Display 1
-    this.display1 = new Display(this.width / 4, 25)
-    // Paddle 2
-    this.p2 = this.getInitialPaddle2()
+
+    // display number
+    this.display1 = new ValueDisplay(this.width / 4, 25)
+    this.display2 = new ValueDisplay(this.width * 3 / 4, 25)
+
+    let paddle1 = this.getInitialPaddle1()
+    this.p1 = new Player(1, 'SomeName1', paddle1)
+    let paddle2 = this.getInitialPaddle2()
+    this.p2 = new Player(2, 'SomeName2', paddle2)
 
     // Display 2
-    this.display2 = new Display(this.width * 3 / 4, 25)
     // Ball
     let ballX = this.width / 2
-    let ballY = this.width / 2
+    let ballY = this.height / 2
     this.ball = new Ball(ballX, ballY)
-    // TODO: Define just one direction
-    this.ball.vy = Math.floor(Math.random() * 12 - 6)
-    this.ball.vx = 7 - Math.abs(this.ball.vy)
+    // this.ball.vy = Math.floor(Math.random() * 12 - 6)
+    this.ball.vx = 5
+    this.ball.vy = 0
   }
 
   /// PUBLIC
+  calculateBounceDirection (collisionPoint) {
+    const paddleMiddle = this.p1.paddle.height / 2
+    const direction = collisionPoint - paddleMiddle
+    // normalize
+    return Math.round(((direction - 0) / paddleMiddle) * 5)
+  }
+
   draw () {
-    drawTime()
+    this.drawTime()
     this.drawPlayField()
     this.drawMiddleLine()
 
     this.ball.draw(this.context)
 
-    this.p1.draw(this.context)
-    this.p2.draw(this.context)
+    this.p1.paddle.draw(this.context)
+    this.p2.paddle.draw(this.context)
+
     this.display1.draw(this.context)
     this.display2.draw(this.context)
   }
@@ -159,6 +150,20 @@ class Game {
 
   drawPlayField () {
     this.context.clearRect(0, 0, this.width, this.height)
+  }
+
+  drawTime () {
+    document.getElementById('timeBox').innerHTML = Math.round(new Date().getTime() / 1000)
+  }
+
+  getDirection () {
+    if (this.keys.isDownPressed()) {
+      return  'DOWN'
+    } else if (this.keys.isUpPressed()) {
+      return 'UP'
+    }
+
+    return 'NOTHING'
   }
 
   getInitialPaddle1 () {
@@ -174,94 +179,84 @@ class Game {
   }
 
   score (player) {
-    // player scores
     player.score++
-    var isPlayer1Winner = player === this.p1
+    var isPlayer1Winner = player.playerNumber === this.p1.playerNumber
 
     // set ball position
     this.ball.x = this.width / 2
-    this.ball.y = player.y + player.height / 2
+    this.ball.y = player.paddle.y + player.paddle.height / 2
 
     // set ball velocity
-    this.ball.vy = Math.floor(Math.random() * 12 - 6)
-    this.ball.vx = 7 - Math.abs(this.ball.vy)
+    this.ball.vx = 5
+    this.ball.vy = 0
+
     if (isPlayer1Winner) {
       this.ball.vx *= -1
     }
   }
 
-  start () {
-    // TODO
-    // based on block heights
+  setNextMove (playerNumber, direction, velocity) {
+    let player = playerNumber === 1 ? this.p1 : this.p2
+
+    if (direction === 'DOWN') { // DOWN
+      player.paddle.y = Math.min(this.height - player.paddle.height, player.paddle.y + velocity)
+    } else if (direction === 'UP') { // UP
+      player.paddle.y = Math.max(0, player.paddle.y - velocity)
+    }
   }
 
   update () {
     // upate ball
-    this.ball.updatePosition()
+    this.ball.setNextPosition()
 
     // update Score
     this.display1.value = this.p1.score
     this.display2.value = this.p2.score
 
-    // TODO: We will make this more generic for both players
-    // Commented out player one
-    // To which Y direction the paddle is moving
-    // if (this.keys.isDownPressed())
-    //   this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4)
-    // } else if (this.keys.isUpPressed()) { // UP
-    //   this.p1.y = Math.max(0, this.p1.y - 4)
-    // }
-
-    this.updatePaddles()
-
-
-    // Top and bottom collision
-    if ((this.ball.vy < 0 && this.ball.y < 0) ||
-    (this.ball.vy > 0 && this.ball.y + this.ball.height > this.height)) {
-      this.ball.vy = -this.ball.vy
-    }
+    this.updateBallMovement()
 
     this.updateScore()
   }
 
-  updatePaddles () {
-    // Trace paddle direction
-    if (this.keys.isDownPressed()) { // DOWN
-      this.p2.y = Math.min(this.height - this.p2.height, this.p2.y + 4)
-    } else if (this.keys.isUpPressed()) { // UP
-      this.p2.y = Math.max(0, this.p2.y - 4)
-    }
-
+  updateBallMovement () {
     if (this.ball.isMovingRight()) {
       // P2 collision detection
       // p2.x behind or at ball && p2.x infront moved ball
-      if (this.p2.x <= this.ball.x + this.ball.width &&
-        this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
-        var collisionDiff = this.ball.x + this.ball.width - this.p2.x
+      if (this.p2.paddle.x <= this.ball.x + this.ball.width &&
+        this.p2.paddle.x > this.ball.x - this.ball.vx + this.ball.width) {
+        var collisionDiff = this.ball.x + this.ball.width - this.p2.paddle.x
         var k = collisionDiff / this.ball.vx
         var y = this.ball.vy * k + (this.ball.y - this.ball.vy)
 
         // collides with right paddle
-        if (y >= this.p2.y && y + this.ball.height <= this.p2.y + this.p2.height) {
-          this.ball.x = this.p2.x - this.ball.width
+        if (y >= this.p2.paddle.y && y + this.ball.height <= this.p2.paddle.y + this.p2.paddle.height) {
+          this.ball.x = this.p2.paddle.x - this.ball.width
           this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy * k)
           this.ball.vx = -this.ball.vx
+          this.ball.vy = this.calculateBounceDirection(this.ball.y - this.p2.paddle.y)
         }
       }
     } else { // ball moves left
       // P1 collision detection
-      if (this.p1.x + this.p1.width >= this.ball.x) {
-        var collisionDiff = this.p1.x + this.p1.width - this.ball.x
+      if (this.p1.paddle.x + this.p1.paddle.width >= this.ball.x) {
+        var collisionDiff = this.p1.paddle.x + this.p1.paddle.width - this.ball.x
         var k = collisionDiff / -this.ball.vx
         var y = this.ball.vy * k + (this.ball.y - this.ball.vy)
 
         // collides with the left paddle
-        if (y >= this.p1.y && y + this.ball.height <= this.p1.y + this.p1.height) {
-          this.ball.x = this.p1.x + this.p1.width
+        if (y >= this.p1.paddle.y && y + this.ball.height <= this.p1.paddle.y + this.p1.paddle.height) {
+          this.ball.x = this.p1.paddle.x + this.p1.paddle.width
           this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy * k)
           this.ball.vx = -this.ball.vx
+          this.ball.vy = this.calculateBounceDirection(this.ball.y - this.p1.paddle.y)
         }
       }
+    }
+
+    // Ball Top and bottom collision
+    if ((this.ball.vy < 0 && this.ball.y < 0) ||
+    (this.ball.vy > 0 && this.ball.y + this.ball.height > this.height)) {
+      this.ball.vy = -this.ball.vy
     }
   }
 
@@ -277,30 +272,91 @@ class Game {
 class StateManager {
     // we need swarm and web3 here
   contructor () {
-
+    this.nextMove = 'NOTHING'
   }
 
-  // getState
+  aggregateMovements () {
+    // TODO separate two teams
+    // get movement of each member
+    let velocity = 0
+    for (let i = 0; i < 5; i++) {
+      // TODO if up then add positive velocity
+      // TODO if down then add minus velocity
+      velocity += 1
+      // velocity -= 1
+    }
+    return {
+      velocity1: velocity,
+      velocity2: velocity
+    }
+  }
+
+  getDirection (velocity) {
+    let direction = 'NOTHING'
+    if (velocity > 0) {
+      direction = 'UP'
+    } else if (velocity < 0) {
+      direction = 'DOWN'
+    }
+    return direction
+  }
+
+  getInitialState () {
+    return {
+      playerNumber1: 1,
+      playerNumber2: 2,
+      direction1: 'NOTHING',
+      direction2: 'NOTHING',
+      velocity1: 0,
+      velocity2: 0
+    }
+  }
+  getState () {
+    // TODO: get and aggregate real state
+    const velocities = this.aggregateMovements()
+    let velocity1 = velocities.velocity1
+    let velocity2 = velocities.velocity2
+
+    const direction1 = this.getDirection(velocity1)
+    const direction2 = this.getDirection(velocity2)
+    return {
+      playerNumber1: 1,
+      playerNumber2: 2,
+      direction1: direction1,
+      direction2: this.nextMove, // TODO change to direction2
+      velocity1: velocity1,
+      velocity2: velocity2
+    }
+  }
+  sendMove (direction) {
+    // TODO send real position
+    this.nextMove = direction
+  }
 }
 
-const game = new Game()
-const stateManager = new StateManager()
+class GameManager {
+  constructor () {
+    this.game = new Game()
+    this.stateManager = new StateManager()
+    this.state = this.stateManager.getInitialState()
+  }
 
-function drawTime () {
-  document.getElementById('timeBox').innerHTML = Math.round(new Date().getTime() / 1000)
+  loop () {
+    let direction = this.game.getDirection()
+    console.log(direction)
+    this.stateManager.sendMove(direction)
+    this.state = this.stateManager.getState()
+    this.game.setNextMove(this.state.playerNumber1, this.state.direction1, this.state.velocity1)
+    this.game.setNextMove(this.state.playerNumber2, this.state.direction2, this.state.velocity2)
+    this.game.update()
+    this.game.draw()
+  }
+
+  start () {
+    window.setInterval(() => { this.loop() }, 100)
+    // setTimeout(() => this.start(), 100)
+  }
 }
 
-function MainLoop () {
-  // trace and send movement
-  // get state
-  // set state
-  // all of above should happen in the the update with the DataFetcher
-  game.update()
-  game.draw()
-
-  // Call the main loop again at a frame rate of 30fps
-  setTimeout(MainLoop, 100)
-}
-
-// Start the game execution
-MainLoop()
+const gameManager = new GameManager()
+gameManager.start()
